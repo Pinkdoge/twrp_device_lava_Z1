908d25ee8c7524513a0227f4a967a8c3069d9c8e
Pinkdoge <Pinkdoge@users.noreply.github.com>
2022-08-09 15:23:28 +0000

    first commit

---
+++ omni_Z1.mk
@@ -0,0 +1,25 @@
+#
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# Inherit from Z1 device
+$(call inherit-product, device/lava/Z1/device.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/omni/config/gsm.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := Z1
+PRODUCT_NAME := omni_Z1
+PRODUCT_BRAND := LAVA
+PRODUCT_MODEL := Z1
+PRODUCT_MANUFACTURER := lava
